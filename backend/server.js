f4c8f7acc928b0836d677c6f7a28c5c5dfe62f00
Ousmane1998 <papeousmanefaye011@gmail.com>
2025-10-19 23:58:20 +0000

    premier commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// 🔌 Connexion à MongoDB locale
+connectDB();
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Hello depuis le backend Node.js connecté à MongoDB 🚀" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Serveur en marche sur le port ${PORT}`);
+});
